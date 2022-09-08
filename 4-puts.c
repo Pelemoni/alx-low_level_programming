97211e5fe4b5fcf858d6f7202fad508b30090d7c
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-08 15:31:25 -0700

    44

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Printingme
+* 
+* Return: Always 0 (Success1) 
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
