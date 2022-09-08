a4c6b9e1876cb55d1e082f3f8c7de186099d28bf
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-08 14:21:48 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,10 @@
 
 /**
  * main - printing in my
-
  *
  * Return Always 0 (Successful me)
- 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");		return (0);
 }
-
-
