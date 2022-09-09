7052c8f868d004753c2942230bdb3edf75624465
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-09 07:20:04 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Printing
  *
- * Return: Always 0 (successful)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts ( " \ "Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like a multilingual puzzle");
+	return(0);
 }
+
