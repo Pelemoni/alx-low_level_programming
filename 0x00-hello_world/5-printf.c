5b60bf773eb1ea9985866234e5a3c479b4af70fd
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-12 04:13:16 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a test
+ * main - Prints a test mine
  *
- * Return: Always 0 (successismine)
+ * Return: Always 0 (successmine)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, 
-\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
 }
