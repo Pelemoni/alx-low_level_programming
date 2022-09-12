2f9d077c8168611ffbe282e9f8a6df6c50a158a3
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-12 03:39:45 -0700

    5

---
+++ 0x00-hello_world/5-prinrf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a test for mee
+ * 
+ * Return: Always 0 (successismine)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
