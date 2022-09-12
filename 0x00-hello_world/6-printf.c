6589c728d5f6b29cdff9d5c78edb44e6a9c72e9c
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-12 03:22:39 -0700

    5

---
+++ 0x00-hello_world/6-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Prints a test mine
+ * *
+ * Return: Always 0 (successmine)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
