a430dc6796c0eb861da02385abce477031dff651
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-12 03:51:18 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -8,6 +7,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, 
+\n");
 	return (0);
 }
