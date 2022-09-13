61f3b822a9dd4e08d059f731508f7cd45ff2767e
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-13 09:22:19 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "holberton.h" 
+/** 
+ * main - horl
+ * 
+ * Return: Always 0 (Successprogrammer)
+ */ 
+int main(void) 
+{ 
+	char *sh = "Holberton"; 
+		    
+	while (*sh) 
+			            {				                     _putchar(*sh); 
+				                 sh++; 
+				    } 					            _putchar('\n'); 					      		return (0);		    }
