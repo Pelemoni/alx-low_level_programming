2864ff234204e2a6f275f69a8fd53e2d2a139a59
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-20 01:34:03 -0700

    222

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *  main - lowercase alphabets printing
+ *  
+ * Return: Always(successprinting)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar (c);
+
+	putchar ('\n');
+	return (0);
+}
