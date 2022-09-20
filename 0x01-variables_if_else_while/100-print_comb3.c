c64b29cb5df9674c2a88891a670b95df21638472
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-20 04:26:16 -0700

    10011000

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '1'; j <= '
-9'; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			if (j > i)
 			{
@@ -21,7 +20,7 @@ int main(void)
 				if (i != 8)
 				 {
 					 putchar (',');
-					 putchar (',');
+					 putchar (' ');
 				}
 			}
 		}
