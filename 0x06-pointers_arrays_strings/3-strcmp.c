841c29389b340ca59a1fba6b6f1dc1bd71af679a
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-22 06:09:39 -0700

    333

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,6 @@
 
 /**
  * _strcmp - Function which compare two strings and
- 
  * @s1: first string
  * @s2: second string
  * Return:
@@ -21,12 +20,12 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
 		else if (s1[i] == '\0')
-		{	
-		diff = s2[i];
-		break;
+		{
+			diff = s2[i];
+			break;
 		}
 		else if (s2[i] == '\0')
-		{	
+		{
 			diff = s1[i];
 			break;
 		}
