d835fcbdd4ce86b0e468dd6d23e9002b66f4e943
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-25 14:09:52 -0700

    11111

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include “main.h”
+
+/**
+ *
+ * print_alphabet – function for print abc me
+ * Return: 0 go
+ */
+
+Void print_alphabet(void)
+{
+	Char c;
+
+	For (c = ‘a’; c <= ‘z’; c++)
+	{
+		_putchar©;
+	}
+	_putchar(‘\n’);
+}
