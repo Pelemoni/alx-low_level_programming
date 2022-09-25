2200d29d5629024fcfaab5f46e832c7bfae8a417
Toheeb Peleowo <abdutoyyib@gmail.com>
2022-09-25 13:50:43 -0700

    00put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,7 @@
 
 /**
  * main - print putchar
- 
- * Return: Always 0 (ss)
+ * Return: always 0
  */
 
 int main(void)
@@ -16,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	_putchar('\n');
+	
 	return (0);
 }
